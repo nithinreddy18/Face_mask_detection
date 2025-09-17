fa2a19e253c6f1a5881636911631017070df51e5
Malluri Nithin Reddy <119342738+nithinreddy18@users.noreply.github.com>
2025-09-17 16:26:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+import datetime
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+# To test for individual images
+mymodel = load_model('mymodel.h5')
+test_image = image.load_img(r'without mask.jpg',target_size=(150, 150))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis=0)
+prediction = mymodel.predict(test_image)[0][0]
+
+# IMPLEMENTING LIVE DETECTION OF FACE MASK
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+while cap.isOpened():
+    _, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+
+    for (x, y, w, h) in faces:
+        face_img = img[y:y + h, x:x + w]
+        cv2.imwrite('temp.jpg', face_img)
+        test_image = image.load_img('temp.jpg', target_size=(150, 150))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        pred = mymodel.predict(test_image)[0][0]
+        if pred == 1:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 3)
+            cv2.putText(img, 'NO MASK', ((x + w) // 2, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+        else:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            cv2.putText(img, 'MASK', ((x + w) // 2, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+        datet = str(datetime.datetime.now())
+        cv2.putText(img, datet, (400, 450), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+    cv2.imshow('img', img)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
